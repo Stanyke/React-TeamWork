fb4ae80ee35c00019cddbce2bf34bc23ef9e3dbe
Stanyke <chibuike.stanley50@gmail.com>
2019-11-25 13:44:23 +0100

    feature(user-login): modify user-auth on login modified Login component [starts]

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -5,6 +5,15 @@ export const Login = () =>
     return(
         <div>
             <h2>Login Form</h2>
+            <p>Sign into your account</p>
+
+            <form className='form' >
+                <div>
+                    <input type='email' placeholder='Enter email address' name='email' />
+                    <br/>
+                    <input type='password' placeholder='*******' name='password' />
+                </div>
+            </form>
         </div>
     );
 };
